0bd4f1ddd13eba972fb2810b900e04300ce23a46
jizhong2 <156254552+jizhong2@users.noreply.github.com>
2024-03-06 14:48:37 -0800

    Update grade.sh

--- grade.sh
+++ grade.sh
@@ -14,3 +14,39 @@ echo 'Finished cloning'
 
 # Then, add here code to compile and run, and do any post-processing of the
 # tests
+
+variable=`find student-submission/ -name "ListExamples.java"`
+
+if [[ -f $variable ]];
+then
+    echo "file found"
+
+else 
+    echo "file not found"
+    exit
+fi
+
+cp $variable TestListExamples.java grading-area
+cp -r lib grading-area
+
+cd grading-area
+
+javac -cp $CPATH *.java
+
+if [ $? -eq 0 ]
+then 
+    echo "Compilation successful"
+
+else
+    echo "Compilation not successful"
+    exit
+fi 
+
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > junit-output.txt
+
+lastline=$(cat junit-output.txt | tail -n 2 | head -n 1)
+tests=$(echo $lastline | awk -F'[, ]' '{print $3}')
+failures=$(echo $lastline | awk -F'[, ]' '{print $6}')
+successes=$((tests - failures))
+
+echo "Your score is $successes / $tests"
